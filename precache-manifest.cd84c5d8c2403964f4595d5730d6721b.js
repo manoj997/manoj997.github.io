e900aa723c4cd9db49b47a4bd002c3ad622b4864
ifs-madhlk <manoj.dharmaraja@ifsworld.com>
2020-07-10 07:59:34 +0530

    Updates

---
+++ precache-manifest.cd84c5d8c2403964f4595d5730d6721b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103fbffe114fd38f5cff2a4decba210a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae3ef20871f6b2bd917",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43224ebc5a4c953d66b7",
+    "url": "/static/js/2.24ef124f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24ef124f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae3ef20871f6b2bd917",
+    "url": "/static/js/main.7b738bb8.chunk.js"
+  },
+  {
+    "revision": "af6a55be2d4c69dd9626",
+    "url": "/static/js/runtime-main.303f8b63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
